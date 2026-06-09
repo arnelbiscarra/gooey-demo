81fc98fd028dec2b58c45735c734736db55dce6e
arnelbiscarra <hdusan05@gmail.com>
2026-06-09 13:32:15 +0800

    Set background color to #EDE8E3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { GooeyTextDemo } from "@/components/demo/gooey-text-demo";
 
 export default function Home() {
   return (
-    <div className="inline-flex items-center justify-center">
+    <div className="inline-flex items-center justify-center" style={{ backgroundColor: "#EDE8E3" }}>
       <GooeyTextDemo />
     </div>
   );
